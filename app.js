279cdb5dcb7485b09002c7276253989cc5f5f094
aleksandr_bagliuk <baglyuk.alexandr@gmail.com>
2021-08-28 17:22:17 +0300

    adding for current work CORS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
